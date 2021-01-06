7699e613bf3097fc6c3994b0671e22356883d751
Dark-Black-36 <77062267+Dark-Black-36@users.noreply.github.com>
2021-01-06 18:25:03 +0330

    Update server.py

--- server.py
+++ server.py
@@ -7,23 +7,25 @@ os.system("clear" or "cls")
 
 print(Fore.CYAN+"""
 
-
-     ,dPYb,                                                                                       
-     IP'`Yb                                                                                       
-     I8  8I                                                                                       
-     I8  8bgg,                                                                                    
-     I8 dP" "8   ,ggg,   gg     gg   ,ggg,,ggg,,ggg,     ,ggggg,    gg      gg    ,g,      ,ggg,  
-     I8d8bggP"  i8" "8i  I8     8I  ,8" "8P" "8P" "8,   dP"  "Y8ggg I8      8I   ,8'8,    i8" "8i 
-     I8P' "Yb,  I8, ,8I  I8,   ,8I  I8   8I   8I   8I  i8'    ,8I   I8,    ,8I  ,8'  Yb   I8, ,8I 
-    ,d8    `Yb, `YbadP' ,d8b, ,d8I ,dP   8I   8I   Yb,,d8,   ,d8'  ,d8b,  ,d8b,,8'_   8)  `YbadP' 
-    88P      Y8888P"Y888P""Y88P"8888P'   8I   8I   `Y8P"Y8888P"    8P'"Y88P"`Y8P' "YY8P8P888P"Y888
-                              ,d8I'                                                               
-                            ,dP'8I                                                                
-                           ,8"  8I                                                                
-                           I8   8I                                                                
-                           `8, ,8I                                                                
-                            `Y8P"                                                                 
-
+******************************************************************************************************
+*                                                                                                    *    
+*     ,dPYb,                                                                                         *
+*     IP'`Yb                                                                                         *
+*     I8  8I                                                                                         *
+*     I8  8bgg,                                                                                      *
+*     I8 dP" "8   ,ggg,   gg     gg   ,ggg,,ggg,,ggg,     ,ggggg,    gg      gg    ,g,      ,ggg,    *
+*     I8d8bggP"  i8" "8i  I8     8I  ,8" "8P" "8P" "8,   dP"  "Y8ggg I8      8I   ,8'8,    i8" "8i   * 
+*     I8P' "Yb,  I8, ,8I  I8,   ,8I  I8   8I   8I   8I  i8'    ,8I   I8,    ,8I  ,8'  Yb   I8, ,8I   * 
+*    ,d8    `Yb, `YbadP' ,d8b, ,d8I ,dP   8I   8I   Yb,,d8,   ,d8'  ,d8b,  ,d8b,,8'_   8)  `YbadP'   *
+*    88P      Y8888P"Y888P""Y88P"8888P'   8I   8I   `Y8P"Y8888P"    8P'"Y88P"`Y8P' "YY8P8P888P"Y888  *
+*                              ,d8I'                                                                 *
+*                            ,dP'8I                                                                  *
+*                           ,8"  8I                                                                  *
+*                           I8   8I                                                                  *
+*                           `8, ,8I                                                                  *
+*                            `Y8P"                                                                   *
+*                                                                                                    *
+******************************************************************************************************
 
 """+Fore.RESET)
 
